7e4bec33f708d85202bad0f50a1c9fa75630dd5e
Greg-Desktop <grets.gergo@gmail.com>
2024-01-06 22:16:24 +0100

    test commit

--- java-backend/src/test/java/com/example/personregistry/service/PersonServiceTest.java
+++ java-backend/src/test/java/com/example/personregistry/service/PersonServiceTest.java
@@ -12,13 +12,10 @@ import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Arrays;
-import java.util.Collections;
 
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
