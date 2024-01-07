212fcf76fe6d50b5e19462b08caf67ead600559a
Greg-Desktop <grets.gergo@gmail.com>
2024-01-07 16:23:09 +0100

    minor fix

--- java-backend/src/test/java/com/example/personregistry/controller/PersonControllerTest.java
+++ java-backend/src/test/java/com/example/personregistry/controller/PersonControllerTest.java
@@ -25,7 +25,6 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
-import static org.assertj.core.api.FactoryBasedNavigableListAssert.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
