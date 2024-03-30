9d0b3ca2bca9b04ab2259e59763cacd68d1a6488
Greg <>
2024-03-30 19:18:19 +0100

    initial files

---
+++ java-backend/src/main/java/com/dpd/personregistrymanager/PersonRegistryManagerApplication.java
@@ -0,0 +1,13 @@
+package com.dpd.personregistrymanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonRegistryManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PersonRegistryManagerApplication.class, args);
+	}
+
+}
---
+++ java-backend/src/test/java/com/dpd/personregistrymanager/PersonRegistryManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dpd.personregistrymanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonRegistryManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
